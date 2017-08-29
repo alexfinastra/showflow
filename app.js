2a5ef08c7c0ac4ceceb494e73b6b0f92ac22b2d9
Alexander Perman <alexp@fundtech.isr>
2017-08-29 08:57:26 +0300

    typo

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ var methodOverride = require('method-override');
 
 var app = express();
 
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
