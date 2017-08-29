521d32b0d4ff017861b61538c6de5ec89d66c7c9
Alexander Perman <alexp@fundtech.isr>
2017-08-29 09:43:09 +0300

    added test oracle.db # 2

--- oracle_connection.js
+++ oracle_connection.js
@@ -33,7 +33,6 @@
 
 var oracledb = require('oracledb');
 
-// Get a non-pooled connection
 oracledb.getConnection(
   {
     user          : "GPP46",
@@ -46,39 +45,15 @@ oracledb.getConnection(
       console.error(err.message);
       return;
     }
-    connection.execute(
-      // The statement to execute
-      "SELECT * " +
-        "FROM interface_types ",
+    console.log('Connection was successful!');
 
-      // The "bind value" 180 for the "bind variable" :id
-      [],
-
-      // Optional execute options argument, such as the query result format
-      // or whether to get extra metadata
-      // { outFormat: oracledb.OBJECT, extendedMetaData: true },
-
-      // The callback function handles the SQL execution results
-      function(err, result)
+    connection.close(
+      function(err)
       {
         if (err) {
           console.error(err.message);
-          doRelease(connection);
           return;
         }
-        console.log(result.metaData); // [ { name: 'DEPARTMENT_ID' }, { name: 'DEPARTMENT_NAME' } ]
-        console.log(result.rows);     // [ [ 180, 'Construction' ] ]
-        doRelease(connection);
       });
   });
 
-// Note: connections should always be released when not needed
-function doRelease(connection)
-{
-  connection.close(
-    function(err) {
-      if (err) {
-        console.error(err.message);
-      }
-    });
-}
