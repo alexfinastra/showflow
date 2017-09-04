139aedab0cc7d95feddd2d15c1fd45b4fe1802c2
Alexander Perman <alexp@fundtech.isr>
2017-09-04 15:41:42 +0300

    added test oracle.db # 59

--- routes/queue.js
+++ routes/queue.js
@@ -153,26 +153,26 @@ router.post('/upload/:id', function(req, res){
   var form = new formidable.IncomingForm();
 
   // specify that we want to allow the user to upload multiple files in a single request
-  form.multiples = true;
+  //form.multiples = true;
 
   // store all uploads in the /uploads directory
-  form.uploadDir = "./env/" +  record["REQUEST_CONNECTIONS_POINT"] // path.join('env', record["REQUEST_CONNECTIONS_POINT"]);  
+  //form.uploadDir = "./env/" +  record["REQUEST_CONNECTIONS_POINT"] // path.join('env', record["REQUEST_CONNECTIONS_POINT"]);  
 
   // every time a file has been uploaded successfully,
   // rename it to it's orignal name
-  form.on('file', function(field, file) {
-    fs.rename(file.path, form.uploadDir + "/" + file.name);
-  });
+  //form.on('file', function(field, file) {
+    //fs.rename(file.path, form.uploadDir + "/" + file.name);
+  //});
 
   // log any errors that occur
-  form.on('error', function(err) {
-    console.log('Upload error F***ed Up: \n' + err);
-  });
+  //form.on('error', function(err) {
+    //console.log('Upload error F***ed Up: \n' + err);
+  //});
 
   // once all the files have been uploaded, send a response to the client
-  form.on('end', function() {
-    res.end('success');
-  });
+  //form.on('end', function() {
+    //res.end('success');
+  //});
 
   // parse the incoming request containing the form data
   form.parse(req);
