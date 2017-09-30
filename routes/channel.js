ade17b96f9eb7006add79cf79e544b6f675b89f1
Alex Perman <alex.perman@gmail.com>
2017-09-30 08:44:53 +0300

    fix fb load data #24

--- routes/channel.js
+++ routes/channel.js
@@ -86,14 +86,14 @@ router.get('/', function (req, res) {
             return;
         }
 
-        connection.execute(query + where, {}, {
+        connection.execute(query, {}, {
             outFormat: oracledb.OBJECT // Return the result as Object
         }, function (err, result) {
             if (err) {
                 res.set('Content-Type', 'application/json');
                 res.status(500).send(JSON.stringify({
                     status: 500,
-                    message: "Error getting the user profile",
+                    message: "Error getting the interfaces profile",
                     detailed_message: err.message
                 }));
             } else {
