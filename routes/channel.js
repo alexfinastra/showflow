0ceeb9c49c1603aa8152d012df6c966776988f23
Alex Perman <alex.perman@gmail.com>
2017-09-30 08:40:09 +0300

    fix fb load data #22

--- routes/channel.js
+++ routes/channel.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var async = require('async')
 var authentication_mdl = require('../middlewares/authentication');
+var oracledb = require('oracledb');
+var dbConfig = require('../db/dbconfig.js');
 
 var Profile = require('../models/profileModel');
 var channel = new Profile('channel'); 
@@ -12,6 +14,116 @@ var identity = {
 	description: 'Data sources or consumers are united into channels. Also grouped by type and ordered by subtype. Auto-refresh, instant monitoring, content validations, documentation reference are applied.'
 }
 
+var query = "SELECT ROWNUM, INTERFACE_NAME, OFFICE, INTERFACE_TYPE, INTERFACE_SUB_TYPE, REQUEST_DIRECTION, MESSAGE_WAIT_STATUS, INTERFACE_STATUS, MESSAGE_STOP_STATUS, STOP_AFTER_CONN_EXCEPTION, INTERFACE_MONITOR_INDEX, REQUEST_PROTOCOL, REQUEST_CONNECTIONS_POINT, REQUEST_FORMAT_TYPE, REQUEST_STORE_IND, RESPONSE_PROTOCOL, RESPONSE_CONNECTIONS_POINT, RESPONSE_FORMAT_TYPE, RESPONSE_STORE_IND, UID_INTERFACE_TYPES, NOT_ACTIVE_BEHAVIOUR, REC_STATUS, ASSOCIATED_SERVICE_NAME, NO_OF_LISTENERS, NON_JMS_RECEPIENT_IND, HANDLER_CLASS, BUSINESS_OBJECT_CLASS, BUSINESS_OPERATION, PMNT_SRC, CUSTOM_PROPERTIES, WAIT_BEHAVIOUR, BATCH_SIZE, SUPPORTED_APP_IDS, BACKOUT_INTERFACE_NAME, BULK_INTERFACE_NAME, APPLICATION_PROTOCOL_TP, REQUEST_GROUP_NM, SEQUENCE_HANDLER_CLASS, RESPONSE_INTERFACE, RESPONSE_TIMEOUT_MILLIS, RESPONSE_TIMEOUT_RETRY_NUM, HEARTBEAT_INTERFACE_NAME, INTERFACE_STOPPED_SOURCE, RESEND_ALLOWED, DESCRIPTION, THROTTLING_TX, THROTTLING_MILLIS, BULKING_PURPOSE, ENABLED_GROUPS,RESUME_SUPPORTED, EVENT_ID_GENERATION, BULK_RESPONSE_BY_ORIG_CHUNK, INVALID_RESPONSE_IN, PCI_DSS_COMPLIANT, BULKING_TRIGGER_METHOD, IS_BULK FROM INTERFACE_TYPES"
+var channel_types = ['ACK', 
+                'AC_IN',
+                'AC_IN_ONUS',
+                'AC_MND_TT1_ONUS_IN',
+                'AC_MND_TT1_OW_IN',
+                'AC_MND_TT2_ONUS_IN',
+                'AC_OUT',
+                'BULK',
+                'BULK_MNDT_FROM_AC',
+                'CEQ',
+                'CLEARING',
+                'CLEARING_ACK_IP_DBTR',
+                'CLEAR_ACK',
+                'CLEAR_ACK_IP',
+                'CLEAR_ACK_SNM',
+                'COMPL_SNM',
+                'CRQ',
+                'CUST_ACK',
+                'DBLCHECK',
+                'DBLCHECK_COMPL',
+                'DEBULK_MSG_IN',
+                'DEBULK_MSG_OUT',
+                'FD_SWIFT',
+                'FEEDER',
+                'FEEDER_TCH',
+                'GFMS',
+                'GPP_TOKEN_VAULT',
+                'IND_MNDT_FROM_AC',
+                'MASSPMNTS',
+                'MNDT_AUTH',
+                'MNDT_AUTH_IN',
+                'MOP',
+                'MQ',
+                'NAK',
+                'NOTIFY',
+                'NPPMSG_IN',
+                'NPRTY_MNDT_FR_CHNL',
+                'PRFL_ACTNS',
+                'PRTY_MNDT_FR_CHNL',
+                'REBULK',
+                'RENTAS2MYR_IN',
+                'RETURN_FUNDS',
+                'RITS',
+                'SPBI',
+                'SPBI_OUT',
+                'SPEI',
+                'SPEI_OUT',
+                'SPI',
+                'SPI_OUT',
+                'STAGING_TASK_IN',
+                'STAGING_TASK_OUT',
+                'SWIFT_IN',
+                'TCH_RTPS_IP_IN',
+                'STAT_DATA'];
+
+var where = " WHERE INTERFACE_TYPE in ("+ channel_types.join(",") +") "
+router.get('/',, function (req, res) {
+    "use strict";
+
+    oracledb.getConnection(dbConfig, function (err, connection) {
+        if (err) {
+            // Error connecting to DB
+            res.set('Content-Type', 'application/json');
+            res.status(500).send(JSON.stringify({
+                status: 500,
+                message: "Error connecting to DB",
+                detailed_message: err.message
+            }));
+            return;
+        }
+
+        connection.execute(query + where, {}, {
+            outFormat: oracledb.OBJECT // Return the result as Object
+        }, function (err, result) {
+            if (err) {
+                res.set('Content-Type', 'application/json');
+                res.status(500).send(JSON.stringify({
+                    status: 500,
+                    message: "Error getting the user profile",
+                    detailed_message: err.message
+                }));
+            } else {
+                res.contentType('application/json').status(200);
+                res.send(JSON.stringify(result.rows));
+            }
+            // Release the connection
+            connection.release(
+                function (err) {
+                    if (err) {
+                        console.error(err.message);
+                    } else {
+                        console.log("GET /user_profiles : Connection released");
+                    }
+                });
+        });
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+/*
 router.get('/', function(req, res, next) {
 	if(oracle == true){
 		channel.load_db();
@@ -28,6 +140,8 @@ router.get('/', function(req, res, next) {
 		res.render('profile_list', { identity: identity, keys: channel.keys() , values: channel.values() });	  
 	}
 });
+*/
+
 
 router.get('/profile/:id', function(req, res, next){			
 	row_id = req.params["id"]	
@@ -40,8 +154,7 @@ router.get('/new', authentication_mdl.is_login, function(req, res, next) {
 	res.render('profile', { title: 'Channel Profile', record: record , model: channel});
 });
 
-var oracledb = require('oracledb');
-var dbConfig = require('../db/dbconfig.js');
+
 // Build UPDATE statement and prepare bind variables
 var buildUpdateStatement = function buildUpdateStatement(req) {
     "use strict";
@@ -100,17 +213,7 @@ var buildUpdateStatement = function buildUpdateStatement(req) {
 };
 
 router.post('/update/:id', function (req, res) {
-    "use strict";
-    /*
-    if ("application/json" !== req.get('Content-Type')) {
-        res.set('Content-Type', 'application/json').status(415).send(JSON.stringify({
-            status: 415,
-            message: "Wrong content-type. Only application/json is supported",
-            detailed_message: null
-        }));
-        return;
-    }
-		*/
+    "use strict";    
     oracledb.getConnection(dbConfig, function (err, connection) {
         if (err) {
             // Error connecting to DB
