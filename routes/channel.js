a0a5fabeac951c5d09f43076adc44290c96c8d87
Alex Perman <alex.perman@gmail.com>
2017-09-30 07:48:53 +0300

    fix fb load data #18

--- routes/channel.js
+++ routes/channel.js
@@ -40,6 +40,120 @@ router.get('/new', authentication_mdl.is_login, function(req, res, next) {
 	res.render('profile', { title: 'Channel Profile', record: record , model: channel});
 });
 
+var oracledb = require('oracledb');
+var dbConfig = require('../db/dbconfig.js');
+// Build UPDATE statement and prepare bind variables
+var buildUpdateStatement = function buildUpdateStatement(req) {
+    "use strict";
+    var statement = "",
+        bindValues = {};
+   
+		var record = channel.select(req.params.id)
+		
+		var key = record["UID_INTERFACE_TYPES"] + ".to_schemas";	
+		channel._properties.set(key, req.body.REQUEST_SCHEMA);	
+		
+		key = record["UID_INTERFACE_TYPES"] + ".from_schemas";	
+		channel._properties.set(key, req.body.RESPONSE_SCHEMA);
+		
+		channel._properties.writeSync();
+
+
+    if (req.body.DESCRIPTION) {
+        statement += "DESCRIPTION = :DESCRIPTION";
+        bindValues.DESCRIPTION = req.body.DESCRIPTION;
+    }
+    if (req.body.REQUEST_DIRECTION) {
+        if (statement) statement = statement + ", ";
+        statement += "REQUEST_DIRECTION = :REQUEST_DIRECTION";
+        bindValues.REQUEST_DIRECTION = req.body.REQUEST_DIRECTION;
+    }
+    if (req.body.REQUEST_PROTOCOL) {
+        if (statement) statement = statement + ", ";
+        statement += "REQUEST_PROTOCOL = :REQUEST_PROTOCOL";
+        bindValues.REQUEST_PROTOCOL = req.body.REQUEST_PROTOCOL;
+    }
+    if (req.body.REQUEST_FORMAT_TYPE) {
+        if (statement) statement = statement + ", ";
+        statement += "REQUEST_FORMAT_TYPE = :REQUEST_FORMAT_TYPE";
+        bindValues.REQUEST_FORMAT_TYPE = req.body.REQUEST_FORMAT_TYPE;
+    }
+    if (req.body.RESPONSE_PROTOCOL) {
+        if (statement) statement = statement + ", ";
+        statement += "RESPONSE_PROTOCOL = :RESPONSE_PROTOCOL";
+        bindValues.RESPONSE_PROTOCOL = req.body.RESPONSE_PROTOCOL;
+    }
+    if (req.body.RESPONSE_FORMAT_TYPE) {
+        if (statement) statement = statement + ", ";
+        statement += "RESPONSE_FORMAT_TYPE = :RESPONSE_FORMAT_TYPE";
+        bindValues.RESPONSE_FORMAT_TYPE = req.body.RESPONSE_FORMAT_TYPE;
+    }
+
+    statement += " WHERE UID_INTERFACE_TYPES = :UID_INTERFACE_TYPES";
+    bindValues.UID_INTERFACE_TYPES = record["UID_INTERFACE_TYPES"];
+    statement = "UPDATE INTERFACE_TYPES SET " + statement;
+
+    return {
+        statement: statement,
+        bindValues: bindValues
+    };
+};
+
+app.post('/update/:id', function (req, res) {
+    "use strict";
+    
+    if ("application/json" !== req.get('Content-Type')) {
+        res.set('Content-Type', 'application/json').status(415).send(JSON.stringify({
+            status: 415,
+            message: "Wrong content-type. Only application/json is supported",
+            detailed_message: null
+        }));
+        return;
+    }
+
+    oracledb.getConnection(connAttrs, function (err, connection) {
+        if (err) {
+            // Error connecting to DB
+            res.set('Content-Type', 'application/json').status(500).send(JSON.stringify({
+                status: 500,
+                message: "Error connecting to DB",
+                detailed_message: err.message
+            }));
+            return;
+        }
+
+        var updateStatement = buildUpdateStatement(req);
+        connection.execute(updateStatement.statement, updateStatement.bindValues, {
+                autoCommit: true,
+                outFormat: oracledb.OBJECT // Return the result as Object
+            },
+            function (err, result) {
+                if (err || result.rowsAffected === 0) {
+                    // Error
+                    res.set('Content-Type', 'application/json');
+                    res.status(400).send(JSON.stringify({
+                        status: 400,
+                        message: err ? "Input Error" : "Profile doesn't exist",
+                        detailed_message: err ? err.message : ""
+                    }));
+                } else {
+                    // Resource successfully updated. Sending an empty response body. 
+                    res.redirect("/channel/profile/" + row_id);
+                }
+                // Release the connection
+                connection.release(
+                    function (err) {
+                        if (err) {
+                            console.error(err.message);
+                        } else {
+                            console.log("POST /channels/update/" + req.params.id + " : Connection released ");
+                        }
+                    });
+            });
+    });
+});
+
+/*
 router.post('/update/:id', function(req, res){
 	row_id = req.params["id"]
 	var record = channel.select(row_id)
@@ -86,5 +200,6 @@ router.post('/update/:id', function(req, res){
 	
 	res.redirect("/channel/profile/" + row_id);
 })
+*/
 
 module.exports = router;
