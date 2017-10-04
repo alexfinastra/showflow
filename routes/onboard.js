3622053352ba8fd789cdd697b061d025bf53f830
Alex Perman <alex.perman@gmail.com>
2017-10-04 08:40:24 +0300

    onboarding db connect #18

--- routes/onboard.js
+++ routes/onboard.js
@@ -125,7 +125,8 @@ var execute = function(file, ind, prefix = ''){
             console.log("Error connecting to DB" + err.message);
             return;
         }
-        console.log( " 5 ========>>>> S Q L :" + line_new.length );        
+        console.log( " 5 ========>>>> S Q L :" + line_new.length ); 
+        /*       
         connection.execute(line_new, [], {
                 autoCommit: true,
                 outFormat: oracledb.OBJECT // Return the result as Object
@@ -149,7 +150,7 @@ var execute = function(file, ind, prefix = ''){
                         }
                     });
             });
-            
+            */
     });
 		}
 	});
