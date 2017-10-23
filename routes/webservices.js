3d852bde4ef10029249e7e934b217dbc9cc07fea
Alex Perman <alex.perman@gmail.com>
2017-10-23 18:03:54 +0300

    added webservices

--- routes/webservices.js
+++ routes/webservices.js
@@ -46,13 +46,128 @@ router.get('/profile/:uid', function(req, res){
 	res.render('service_profile', { title: 'SOAP Service - ' + services.get(req.params.uid + ".flow_item.interface_type"), service: services.get(req.params.uid), uid: req.params.uid });
 })
 
-router.post('/execute/:uid', function(req, res){
-	uid = req.params.uid
+
+
+
+// Build Input Parameters 
+var buildInput2SOAP = function buildInput2SOAP(req, uid) {
+    "use strict";
+    var bindValues = {};
+    var properties = new json.File(appRoot + "/db/properties/services_index.json" ); 
+    properties.readSync(); 
+    
+    var req_fields = properties.get(uid + ".req_fields");
+    if (req_fields.indexOf("UserID,") > -1 && req.body.UserID) {        
+        bindValues.UserID = req.body.UserID;
+    }
+    if (req_fields.indexOf("effectiveDate,") > -1 && req.body.effectiveDate) {        
+        bindValues.effectiveDate = req.body.effectiveDate;
+    }
+    if (req_fields.indexOf("profileIDs,") > -1 && req.body.profileIDs) {        
+        bindValues.profileIDs = req.body.profileIDs;
+    }
+    if (req_fields.indexOf("profileID,") > -1 && req.body.profileID) {        
+        bindValues.profileID = req.body.profileID;
+    }
+    if (req_fields.indexOf("profileRecordUniqueID,") > -1 && req.body.profileRecordUniqueID) {        
+        bindValues.profileRecordUniqueID = req.body.profileRecordUniqueID;
+    }
+    if (req_fields.indexOf("P_MID,") > -1 && req.body.P_MID) {        
+        bindValues.P_MID = req.body.P_MID;
+    }
+    if (req_fields.indexOf("P_OFFICE,") > -1 && req.body.P_OFFICE) {        
+        bindValues.P_OFFICE = req.body.P_OFFICE;
+    }
+    if (req_fields.indexOf("P_DEPARTMENT,") > -1 && req.body.P_DEPARTMENT) {        
+        bindValues.P_DEPARTMENT = req.body.P_DEPARTMENT;
+    }
+    if (req_fields.indexOf("D_BUTTON_ID,") > -1 && req.body.D_BUTTON_ID) {        
+        bindValues.D_BUTTON_ID = req.body.D_BUTTON_ID;
+    }
+    return bindValues
+};
+
+
+
+
+
+router.post('/execute/:uid', function(req, res){	
   var services = new json.File(appRoot + "/db/properties/services_index.json" );
   services.readSync();
-
-  
-	res.redirect('/webservices/profile/'+uid);
+ 
+  var key = req.params.uid; 
+  var url = services.get(key + ".wsdl")
+  var args = buildInput2SOAP(req, uid);
+  console.log(">>>>> URL " + url + " Input to SOAP :" + JSON.stringify(args) )
+	
+  soap.createClient(url, function(err, client) {
+      if (services.get(key+".func_name").indexOf("applyChanges") > -1){
+        client.applyChanges(args, function(err, result) {
+          console.log(result);
+          res.redirect('/webservices/profile/'+uid);
+        });
+      }
+      if (services.get(key+".func_name").indexOf("performLoadProfile") > -1){
+        client.performLoadProfile(args, function(err, result) {
+          console.log(result);
+          res.redirect('/webservices/profile/'+uid);
+        });
+      }
+      if (services.get(key+".func_name").indexOf("profileSave,") > -1){
+        client.profileSave(args, function(err, result) {
+          console.log(result);
+          res.redirect('/webservices/profile/'+uid);
+        });
+      }
+      if (services.get(key+".func_name").indexOf("profileHold") > -1){
+        client.profileHold(args, function(err, result) {
+          console.log(result);
+          res.redirect('/webservices/profile/'+uid);
+        });
+      }
+      if (services.get(key+".func_name").indexOf("profileDelete,") > -1){
+        client.profileDelete(args, function(err, result) {
+          console.log(result);
+          res.redirect('/webservices/profile/'+uid);
+        });
+      }
+      if (services.get(key+".func_name").indexOf("profileActivate,") > -1){
+        client.profileActivate(args, function(err, result) {
+          console.log(result);
+          res.redirect('/webservices/profile/'+uid);
+        });
+      }
+      if (services.get(key+".func_name").indexOf("profileDecline,") > -1){
+        client.profileDecline(args, function(err, result) {
+          console.log(result);
+          res.redirect('/webservices/profile/'+uid);
+        });
+      }
+      if (services.get(key+".func_name").indexOf("profileApprove,") > -1){
+        client.profileApprove(args, function(err, result) {
+          console.log(result);
+          res.redirect('/webservices/profile/'+uid);
+        });
+      }
+      if (services.get(key+".func_name").indexOf("profileMultiActions,") > -1){
+        client.profileMultiActions(args, function(err, result) {
+          console.log(result);
+          res.redirect('/webservices/profile/'+uid);
+        });
+      }
+      if (services.get(key+".func_name").indexOf("loadMessage,") > -1){
+        client.loadMessage(args, function(err, result) {
+          console.log(result);
+          res.redirect('/webservices/profile/'+uid);
+        });
+      }
+      if (services.get(key+".func_name").indexOf("submitMessage,") > -1){
+        client.submitMessage(args, function(err, result) {
+          console.log(result);
+          res.redirect('/webservices/profile/'+uid);
+        });
+      }
+  });
 })
 
 router.get('/populate/wsdl', function(req, res){
@@ -126,6 +241,7 @@ router.get('/populate/init', function(req, res){
         "active": false,
         "connected": false,
         "req_fields": "",
+        "func_name": "",
         "res_fields": "",
         "flow_item": {
           "step": 0,
